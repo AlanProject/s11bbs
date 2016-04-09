24a397edf490566dcd2232f5e24c0c8e26566dbb
alan <alanwmail@163.com>
2016-04-09 17:57:53 +0800

    uploads

--- s11bbs.sql
+++ s11bbs.sql
@@ -125,7 +125,7 @@ CREATE TABLE `auth_user` (
 
 LOCK TABLES `auth_user` WRITE;
 /*!40000 ALTER TABLE `auth_user` DISABLE KEYS */;
-INSERT INTO `auth_user` VALUES (1,'pbkdf2_sha256$24000$zdqiklxyDzsW$6hKOPYA79q1a79hYSlMKGjss2saNFEINlJ+bHizy7x8=','2016-03-30 08:13:48',1,'alan','','','',1,1,'2016-03-30 08:13:29'),(2,'pbkdf2_sha256$24000$EBiQyncMz5Jy$6+nCUpovkdHKrqQQJThvLzMB1EamfX10j/vMKKygK1M=',NULL,0,'lisa','','','',0,1,'2016-03-30 08:24:37');
+INSERT INTO `auth_user` VALUES (1,'pbkdf2_sha256$24000$zdqiklxyDzsW$6hKOPYA79q1a79hYSlMKGjss2saNFEINlJ+bHizy7x8=','2016-04-09 09:54:20',1,'alan','','','',1,1,'2016-03-30 08:13:29'),(2,'pbkdf2_sha256$24000$EBiQyncMz5Jy$6+nCUpovkdHKrqQQJThvLzMB1EamfX10j/vMKKygK1M=',NULL,0,'lisa','','','',0,1,'2016-03-30 08:24:37');
 /*!40000 ALTER TABLE `auth_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -206,7 +206,7 @@ CREATE TABLE `django_admin_log` (
   KEY `django_admin_log_user_id_c564eba6_fk_auth_user_id` (`user_id`),
   CONSTRAINT `django_admin_log_user_id_c564eba6_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`),
   CONSTRAINT `django_admin__content_type_id_c4bce8eb_fk_django_content_type_id` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -215,7 +215,7 @@ CREATE TABLE `django_admin_log` (
 
 LOCK TABLES `django_admin_log` WRITE;
 /*!40000 ALTER TABLE `django_admin_log` DISABLE KEYS */;
-INSERT INTO `django_admin_log` VALUES (1,'2016-03-30 08:14:51','1','admin',1,'Added.',12,1),(2,'2016-03-30 08:14:58','2','guest',1,'Added.',12,1),(3,'2016-03-30 08:17:53','1','Alan',1,'Added.',11,1),(4,'2016-03-30 08:20:31','1','notice',1,'Added.',10,1),(5,'2016-03-30 08:20:41','2','hot',1,'Added.',10,1),(6,'2016-03-30 08:21:20','1','alan test author:Alan',1,'Added.',7,1),(7,'2016-03-30 08:22:27','2','notice test author:Alan',1,'Added.',7,1),(8,'2016-03-30 08:22:53','1','hahahahahhahah user:Alan',1,'Added.',8,1),(9,'2016-03-30 08:23:02','2','woquuuaa user:Alan',1,'Added.',8,1),(10,'2016-03-30 08:23:13','3','dadadadadadadad user:Alan',1,'Added.',8,1),(11,'2016-03-30 08:23:24','4','goood user:Alan',1,'Added.',8,1),(12,'2016-03-30 08:24:37','2','lisa',1,'Added.',4,1),(13,'2016-03-30 08:24:40','2','lisa',1,'Added.',11,1);
+INSERT INTO `django_admin_log` VALUES (1,'2016-03-30 08:14:51','1','admin',1,'Added.',12,1),(2,'2016-03-30 08:14:58','2','guest',1,'Added.',12,1),(3,'2016-03-30 08:17:53','1','Alan',1,'Added.',11,1),(4,'2016-03-30 08:20:31','1','notice',1,'Added.',10,1),(5,'2016-03-30 08:20:41','2','hot',1,'Added.',10,1),(6,'2016-03-30 08:21:20','1','alan test author:Alan',1,'Added.',7,1),(7,'2016-03-30 08:22:27','2','notice test author:Alan',1,'Added.',7,1),(8,'2016-03-30 08:22:53','1','hahahahahhahah user:Alan',1,'Added.',8,1),(9,'2016-03-30 08:23:02','2','woquuuaa user:Alan',1,'Added.',8,1),(10,'2016-03-30 08:23:13','3','dadadadadadadad user:Alan',1,'Added.',8,1),(11,'2016-03-30 08:23:24','4','goood user:Alan',1,'Added.',8,1),(12,'2016-03-30 08:24:37','2','lisa',1,'Added.',4,1),(13,'2016-03-30 08:24:40','2','lisa',1,'Added.',11,1),(14,'2016-04-09 09:09:37','3','IntegerFiel author:Alan',3,'',7,1),(15,'2016-04-09 09:09:37','1','alan test author:Alan',3,'',7,1),(16,'2016-04-09 09:09:37','2','notice test author:Alan',3,'',7,1);
 /*!40000 ALTER TABLE `django_admin_log` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -258,7 +258,7 @@ CREATE TABLE `django_migrations` (
   `name` varchar(255) NOT NULL,
   `applied` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -267,7 +267,7 @@ CREATE TABLE `django_migrations` (
 
 LOCK TABLES `django_migrations` WRITE;
 /*!40000 ALTER TABLE `django_migrations` DISABLE KEYS */;
-INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2016-03-30 08:09:32'),(2,'auth','0001_initial','2016-03-30 08:09:34'),(3,'admin','0001_initial','2016-03-30 08:09:34'),(4,'admin','0002_logentry_remove_auto_add','2016-03-30 08:09:34'),(5,'contenttypes','0002_remove_content_type_name','2016-03-30 08:09:35'),(6,'auth','0002_alter_permission_name_max_length','2016-03-30 08:09:35'),(7,'auth','0003_alter_user_email_max_length','2016-03-30 08:09:35'),(8,'auth','0004_alter_user_username_opts','2016-03-30 08:09:35'),(9,'auth','0005_alter_user_last_login_null','2016-03-30 08:09:35'),(10,'auth','0006_require_contenttypes_0002','2016-03-30 08:09:35'),(11,'auth','0007_alter_validators_add_error_messages','2016-03-30 08:09:35'),(12,'sessions','0001_initial','2016-03-30 08:09:36'),(13,'web','0001_initial','2016-03-30 08:09:40'),(14,'web','0002_auto_20160330_1617','2016-03-30 08:17:43');
+INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2016-03-30 08:09:32'),(2,'auth','0001_initial','2016-03-30 08:09:34'),(3,'admin','0001_initial','2016-03-30 08:09:34'),(4,'admin','0002_logentry_remove_auto_add','2016-03-30 08:09:34'),(5,'contenttypes','0002_remove_content_type_name','2016-03-30 08:09:35'),(6,'auth','0002_alter_permission_name_max_length','2016-03-30 08:09:35'),(7,'auth','0003_alter_user_email_max_length','2016-03-30 08:09:35'),(8,'auth','0004_alter_user_username_opts','2016-03-30 08:09:35'),(9,'auth','0005_alter_user_last_login_null','2016-03-30 08:09:35'),(10,'auth','0006_require_contenttypes_0002','2016-03-30 08:09:35'),(11,'auth','0007_alter_validators_add_error_messages','2016-03-30 08:09:35'),(12,'sessions','0001_initial','2016-03-30 08:09:36'),(13,'web','0001_initial','2016-03-30 08:09:40'),(14,'web','0002_auto_20160330_1617','2016-03-30 08:17:43'),(15,'web','0003_article_summery','2016-04-07 07:41:48'),(16,'web','0004_auto_20160409_1507','2016-04-09 07:07:22');
 /*!40000 ALTER TABLE `django_migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -293,7 +293,7 @@ CREATE TABLE `django_session` (
 
 LOCK TABLES `django_session` WRITE;
 /*!40000 ALTER TABLE `django_session` DISABLE KEYS */;
-INSERT INTO `django_session` VALUES ('c9d6kk5zqdrh9kv8g1bi1rrjdomrdzkm','NTcwNDU5YWExMWQzMWEzYWM1OTE0NDBlYWE4OWY2YWJjZjRlNTM5Zjp7Il9hdXRoX3VzZXJfaGFzaCI6IjUwMjcwODkxN2I0MjdiOGE5OTQzYTRmNzI3ZThlMDA1ZjRiNTVjNDAiLCJfYXV0aF91c2VyX2JhY2tlbmQiOiJkamFuZ28uY29udHJpYi5hdXRoLmJhY2tlbmRzLk1vZGVsQmFja2VuZCIsIl9hdXRoX3VzZXJfaWQiOiIxIn0=','2016-04-13 08:13:48');
+INSERT INTO `django_session` VALUES ('9i9nv1zauay6hiqtcicjjyfj5oke8k98','NTcwNDU5YWExMWQzMWEzYWM1OTE0NDBlYWE4OWY2YWJjZjRlNTM5Zjp7Il9hdXRoX3VzZXJfaGFzaCI6IjUwMjcwODkxN2I0MjdiOGE5OTQzYTRmNzI3ZThlMDA1ZjRiNTVjNDAiLCJfYXV0aF91c2VyX2JhY2tlbmQiOiJkamFuZ28uY29udHJpYi5hdXRoLmJhY2tlbmRzLk1vZGVsQmFja2VuZCIsIl9hdXRoX3VzZXJfaWQiOiIxIn0=','2016-04-23 09:54:20');
 /*!40000 ALTER TABLE `django_session` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -313,14 +313,15 @@ CREATE TABLE `web_article` (
   `hidden` tinyint(1) NOT NULL,
   `priority` int(11) NOT NULL,
   `author_id` int(11) NOT NULL,
-  `categroy_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `summery` varchar(25) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `title` (`title`),
   KEY `web_article_4f331e2f` (`author_id`),
-  KEY `web_article_5bf698f4` (`categroy_id`),
-  CONSTRAINT `web_article_categroy_id_ad0f6811_fk_web_category_id` FOREIGN KEY (`categroy_id`) REFERENCES `web_category` (`id`),
+  KEY `web_article_5bf698f4` (`category_id`),
+  CONSTRAINT `web_article_category_id_427b8715_fk_web_category_id` FOREIGN KEY (`category_id`) REFERENCES `web_category` (`id`),
   CONSTRAINT `web_article_author_id_de2be06b_fk_web_userprofile_id` FOREIGN KEY (`author_id`) REFERENCES `web_userprofile` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -329,7 +330,7 @@ CREATE TABLE `web_article` (
 
 LOCK TABLES `web_article` WRITE;
 /*!40000 ALTER TABLE `web_article` DISABLE KEYS */;
-INSERT INTO `web_article` VALUES (1,'alan test','uploads/QQ图片20160329173910.png','This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It','16:21:20',0,1000,1,2),(2,'notice test','uploads/QQ图片20160329173910_tpJGv9x.png','This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It','16:22:27',0,1000,1,1);
+INSERT INTO `web_article` VALUES (4,'上海交大校庆92岁院士演讲 推开为其准备的椅子','/static/imgs/1/133280344.jpg','<p>&ldquo;在惊涛骇浪的孤岛，他埋下头，甘心做沉默的砥柱；在一穷二白的年代，他挺起胸，成为国家最大的财富。他的人生，正如深海中的潜艇，无声，但有无穷的力量。&rdquo;</p>\r\n\r\n<p>　　在120周年校庆之际，上海交通大学官方微博8日转发了一张照片，在校庆活动中，92岁的老学长，在演讲时毅然推开了为他准备的椅子，他就是上海交大1949届造船系校友、中国核潜艇之父、中国工程院院士黄旭华。</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p><img alt=\"上海交大校庆92岁院士演讲 推开为其准备的椅子\" src=\"http://img1.gtimg.com/news/pics/hv1/4/173/2049/133280344.jpg\" /></p>\r\n\r\n<p>　　黄旭华，中国工程院院士，我国第一代核动力潜艇研制创始人之一。原籍广东省揭阳市揭东县玉湖镇新寮村，1924年2月24日出生于广东省汕尾市海丰县田墘镇。1949年毕业于国立交通大学造船系船舶制造专业，先后从事过民用船舶和军用舰艇的研究设计工作，1958年开始参与并领导我国第一代核潜艇的研究设计工作。先后出任第一代核潜艇副总设计师、第二任总设计师，历任中船重工集团公司副所长、所长、党委书记。曾先后于1978年获全国科学大会奖、1982年获国防科工委二等奖。他参与完成的我国第一代核潜艇研制获1985年国家科学技术进步奖特等奖、导弹核潜艇研制获1996年国家科学技术进步奖特等奖。1989年被评为全国先进工作者。1994年当选为中国工程院院士。2014年1月当选中央电视台感动中国2013年度人物。</p>\r\n\r\n<p>　　&ldquo;若有人问我们如何评价这一生，我们会说，此生没有虚度，我们的这一生都奉献给国家、给核潜艇事业，我们仅用不到10年的时间就实现了毛主席&lsquo;核潜艇，一万年也要造出来&rsquo;的誓言，我们此生无悔！&rdquo; &mdash;&mdash;核动力潜艇总设计师黄旭华。</p>','15:39:12',1,1000,1,2,'“在惊涛骇浪的孤岛，他挺起胸.....'),(5,'\"海淘\"税改首日:多家电商宣布包税 被指贴钱占位','/static/imgs/1/20160409075619234.jpg','<p>　（原标题：&ldquo;海淘&rdquo;税改首日 多家电商宣布&ldquo;包税&rdquo;打响补贴大战(组图)）</p>\r\n\r\n<p><img src=\"http://himg2.huanqiu.com/attachment2010/2016/0409/20160409075619234.jpg\" /></p>\r\n\r\n<p>妈妈们最爱海淘的奶粉在&ldquo;正面清单&rdquo;里。</p>\r\n\r\n<p>　　<strong>《&ldquo;海淘&rdquo;免税终结部分电商已调价》追踪</strong></p>\r\n\r\n<p>　　4月8日是跨境电商税改实施首日，跨境电商零售进口商品不再按邮递物品征收行邮税，而是按货物征收关税和进口环节增值税、消费税。</p>\r\n\r\n<p>　　就在4月7日晚间，财政部等11部门公布了《跨境电子商务零售进口商品清单》，也就是俗称的&ldquo;正面清单&rdquo;，共涵盖1142个8位税号商品。</p>\r\n\r\n<p>　　税改第一天，电商们在忙什么？他们的感受是什么？在这个特殊的节点上，<a href=\"http://country.huanqiu.com/china\" target=\"_blank\">中国</a>跨境电子商务峰会在成都举行，记者采访了波罗蜜、汇通天下等多家涉及跨境电商的企业。</p>\r\n\r\n<p>　　多家企业的负责人都告诉记者，前夜几乎无人入睡。一方面是忙着改造升级系统、清查产品，以保证商品能够符合7日夜间出台的进口清单。另一方面，未来跨境电商怎么走？&ldquo;补贴战&rdquo;是否一触即发？这些都是他们不得不考虑的问题。</p>\r\n\r\n<p>　　<strong>进口清单出炉</strong></p>\r\n\r\n<p>　　<strong>1142个8位税号商品免交许可证</strong></p>\r\n\r\n<p>　　4月7日晚间公布的清单，共涵盖1142个8位税号商品，主要是国内有一定消费需求，可满足相关部门监管要求，且客观上能够以快件、邮件等方式进境的生活消费品，包括部分食品饮料、服装鞋帽、家用电器以及部分化妆品、纸尿裤、儿童玩具、保温杯等。</p>\r\n\r\n<p>　　根据相关主管部门的意见，清单内的商品将免于向海关提交许可证件，检验检疫监督管理按照国家相关法律法规的规定执行；直购商品免于验核通关单，网购保税商品&ldquo;一线&rdquo;进区时须按货物验核通关单、&ldquo;二线&rdquo;出区时免于验核通关单。</p>\r\n\r\n<p>　　&ldquo;我从事外贸进出口相关业务将近30年，从来没有见过一项政策的出台是那么的艰难，那么的纠结！就算经过两年的试点磨合，今时今日正式开始实施新政，很多资深从业者依然没有搞清楚实施的细节。&rdquo;汇通天下CEO孙剑巍说，7日晚间进口清单才出台，为了能在8日合规营业，不少电商都忙于改系统，&ldquo;很多老大从昨天开始就在公司里没停过，可能现在都还没有睡觉。&rdquo;</p>','15:59:02',1,1000,1,2,'4月8日是跨境电商税改实施首日'),(6,'沈海高速发生重大交通事故 已致8死17伤','/static/imgs/1/1460163207939560.jpg','<h1>沈海高速发生重大交通事故 已致8死17伤</h1>\r\n\r\n<h2>&nbsp;</h2>\r\n\r\n<p>2016-04-09 08:53:07来源：新华社</p>\r\n\r\n<p>分享</p>\r\n\r\n<p>字号:大中小</p>\r\n\r\n<p><img src=\"http://images.haiwainet.cn/20160409/1460163206213817.jpg\" /></p>\r\n\r\n<p>沈海高速发生重大交通事故 已致8死17伤</p>\r\n\r\n<p>4月9日上午，青岛市公安局交通警察支队官方微博发布消息，9日凌晨1时许，在沈海高速北向南方向618KM+900M处发生一辆长途卧铺客车与两辆货车相撞的交通事故，造成8人死亡，17人受伤。事故调查及善后处置等工作正在进行中。</p>\r\n\r\n<p>【相关报道】</p>\r\n\r\n<p>微博网友@苏州名流 ：【从苏州开车到青岛出差遭遇严重车祸[伤心][伤心]】凌晨1点左右，沈海高速胶州段，一辆满载瓷砖大货车失控与一辆大客车激烈碰撞。造成两车头完全损毁。</p>\r\n\r\n<p><img src=\"http://images.haiwainet.cn/20160409/1460163207245101.jpg\" /></p>\r\n\r\n<p><img src=\"http://images.haiwainet.cn/20160409/1460163207939560.jpg\" /></p>\r\n\r\n<p><img src=\"http://images.haiwainet.cn/20160409/1460163207114826.jpg\" /></p>\r\n\r\n<p><img src=\"http://images.haiwainet.cn/20160409/1460163207469434.jpg\" /></p>','17:39:13',1,1000,1,1,'4月9日上午，青岛市公安局交通警察支队');
 /*!40000 ALTER TABLE `web_article` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -417,7 +418,6 @@ CREATE TABLE `web_comment` (
 
 LOCK TABLES `web_comment` WRITE;
 /*!40000 ALTER TABLE `web_comment` DISABLE KEYS */;
-INSERT INTO `web_comment` VALUES (1,'hahahahahhahah','16:22:53',1,NULL,1),(2,'woquuuaa','16:23:02',1,1,1),(3,'dadadadadadadad','16:23:13',1,2,1),(4,'goood','16:23:24',2,NULL,1);
 /*!40000 ALTER TABLE `web_comment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -540,4 +540,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-03-30 18:21:54
+-- Dump completed on 2016-04-09 17:56:13
