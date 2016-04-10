a4d738fa68ab91e1c6063a340b270fc5ed090340
alan <alanwmail@163.com>
2016-04-10 17:56:53 +0800

    uploads

--- web/templatetags/comment.py
+++ web/templatetags/comment.py
@@ -12,13 +12,12 @@ def deep_search(comment_dic,i):
 def html_views(comment_dic,margin_left):
     html = ""
     for k,v in comment_dic.items():
-        html +="<div style='margin-left:%spx' class='comment_node'>"%margin_left+k.comment+"</div>"
+        html +="<div style='margin-left:%spx' class='comment_node'>"%margin_left+"<p>"+str(k.user)+'&nbsp&nbsp'+k.comment+"&nbsp&nbsp"+str(k.date)+"</p>"+"</div>"
         if v:
             html += html_views(v,margin_left+15)
     return html
 @register.simple_tag
 def comment_tree(comment_tuple):
-
     comment_dic = {}
     for i in comment_tuple:
         if i.parent_comment is None:
@@ -28,7 +27,7 @@ def comment_tree(comment_tuple):
     html = "<div class='comment_box'>"
     margin_left = 0
     for k,v  in comment_dic.items():
-        html +="<div class='comment_node'>"+k.comment+"</div>"
+        html +="<div class='comment_node'>"+"<p>"+str(k.user)+'&nbsp&nbsp'+k.comment+"&nbsp&nbsp"+str(k.date)+"<p>"+"</div>"
         html+= html_views(v,margin_left+15)
     html += "</div>"
     return html
